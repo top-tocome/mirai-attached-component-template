ce612c9f76e87df0e2cc08a8425d54ea21bcf91d
remu <986563206@qq.com>
2021-07-28 16:33:43 +0800

    mirai-console-plugin-template

---
+++ settings.gradle.kts
@@ -0,0 +1,2 @@
+rootProject.name = "mirai-attached-component-template"
+include(":mirai-command-component-framework")
---
+++ src/test/kotlin/RunTerminal.kt
@@ -0,0 +1,20 @@
+package top.tocome.mirai.plugin
+
+import net.mamoe.mirai.alsoLogin
+import net.mamoe.mirai.console.MiraiConsole
+import net.mamoe.mirai.console.plugin.PluginManager.INSTANCE.enable
+import net.mamoe.mirai.console.plugin.PluginManager.INSTANCE.load
+import net.mamoe.mirai.console.terminal.MiraiConsoleTerminalLoader
+import net.mamoe.mirai.console.util.ConsoleExperimentalApi
+
+@ConsoleExperimentalApi
+suspend fun main() {
+    MiraiConsoleTerminalLoader.startAsDaemon()
+
+    JavaPluginMain.INSTANCE.load()// 主动加载插件, Console 会调用 plugin.onLoad
+    JavaPluginMain.INSTANCE.enable()// 主动启用插件, Console 会调用 plugin.onEnable
+
+//    val bot = MiraiConsole.addBot(123456, "").alsoLogin() // 登录一个测试环境的 Bot
+
+    MiraiConsole.job.join()
+}
